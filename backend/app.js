d642fe17d224d506aaab98e183da18d2a1e817a0
Samet Irkören <sametirkoren@gmail.com>
2020-12-27 14:17:19 +0300

    error middleware

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+const errorMiddleware = require("./middlewares/errors");
 app.use(express.json());
 
 // Bütün routeları tanımlıyoruz.
@@ -8,5 +9,9 @@ app.use(express.json());
 const products = require('./routes/product');
 
 app.use('/api/v1',products);
+
+// Hataları ele aldığımız middleware'imiz
+
+app.use(errorMiddleware);
 
 module.exports = app
